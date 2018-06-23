e90f6fac892a7de6896c8b963613d8d9f439429b
Alex Zhang <alexgzhang92@gmail.com>
2018-06-23 20:35:01 +0000

    automatically backed up by learn

--- app/models/ship.rb
+++ app/models/ship.rb
@@ -1,2 +1,15 @@
 class Ship
+  attr_accessor :name, :type, :booty
+  
+  SHIPS = []
+  
+  def self.all
+    SHIPS
+  end
+  
+  def initialize(args)
+    @name = args[:name]
+    @type = args[:type]
+    @booty = args[:booty]
+  end
 end
